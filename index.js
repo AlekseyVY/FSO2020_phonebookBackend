81f551a7a2decdfcbcf21d7449537ae41c564c22
AlekseyVY <alekseyVY@gmail.com>
2020-05-14 16:51:47 +0400

    phonebook_backend step_1

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('backend server')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+
+const persons = [
+    {
+        name: "Arto Helias",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-521235431",
+        id: 4
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-1543198",
+        id: 3
+    },
+    {
+        name: "Mary Popendick",
+        number: "39-23-9078642",
+        id: 6
+    }
+]
+
+
+app.get('/api/persons', (request, response) => {
+    return response.json(persons)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
